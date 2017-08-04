47ec0b3276432e6482cb13431239bd07b1304555
csvinhal <cristiano.vinhal10@gmail.com>
2017-08-04 11:06:42 -0300

    add filter

--- src/app/components/grid/grid.component.js
+++ src/app/components/grid/grid.component.js
@@ -3,27 +3,41 @@ import template from './grid.html';
 
 class GridController {
 
-	constructor($scope, $state, generatorService, authService, modalService) {
+	constructor($scope, $state, uiGridConstants) {
 		'ngInject';
 		this.$scope = $scope;
-		this.modalService = modalService;
+		this.uiGridConstants = uiGridConstants;
 		this.initializer();
 	}
 
 	initializer() {
+		this.highlightFilteredHeader = (row, rowRenderIndex, col, colRenderIndex) => {
+			if (col.filters[0].term) return 'header-filtered';
+			else return '';
+		};
+
 		this.gridOptions = {};
+		this.gridOptions.enableFiltering = true;
 		this.gridOptions.columnDefs = [
-			{ name: 'serviceOrder', width: '*' },
-			{ name: 'description', width: '*' },
-			{ name: 'tab', width: '*' },
-			{ name: 'milestone', width: '*' },
-			{ name: 'function', width: '*' },
-			{ name: 'status', width: '*' },
-			{ name: 'expected', width: '*' },
-			{ name: 'activeStartedDate', width: '*' },
-			{ name: 'minutesExpected', width: '*' },
-			{ name: 'observation', width: '*' },
-			{ name: 'problem?', width: '*' }
+			{ name: 'serviceOrder', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{ name: 'description', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{ name: 'tab', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{ name: 'milestone', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{
+				name: 'function', width: '*', headerCellClass: this.highlightFilteredHeader
+			},
+			{
+				name: 'status', width: '*', filter: {
+					type: this.uiGridConstants.filter.SELECT,
+					selectOptions: [{ value: '1', label: 'Documentação' }, { value: '2', label: 'Programação' }, { value: '3', label: 'Concluída' }, { value: '4', label: 'Teste' }, { value: '5', label: 'Impedimento' }, { value: '6', label: 'Falta Vídeo' }]
+				},
+				headerCellClass: this.highlightFilteredHeader
+			},
+			{ name: 'expected', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{ name: 'activeStartedDate', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{ name: 'minutesExpected', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{ name: 'observation', width: '*', headerCellClass: this.highlightFilteredHeader },
+			{ name: 'problem?', width: '*', headerCellClass: this.highlightFilteredHeader }
 		];
 
 		this.gridOptions.data = [
@@ -42,11 +56,11 @@ class GridController {
 				"problem?": "Não"
 			},
 			{
-				"serviceOrder": "1405799",
+				"serviceOrder": "1405798",
 				"description": "#PRG 1 - 833728 - Gestão de consignados - Gestão de consignados",
 				"tab": "Gestão de consignados",
 				"milestone": "Milestone 1",
-				"function": "ComSol_FS",
+				"function": "ComSol_FC",
 				"status": "Concluída",
 				"expected": "Sim",
 				"activeStartedDate": "07/06/2017",
@@ -56,12 +70,12 @@ class GridController {
 				"problem?": "Não"
 			},
 			{
-				"serviceOrder": "1405799",
+				"serviceOrder": "1405797",
 				"description": "#PRG 1 - 833728 - Gestão de consignados - Gestão de consignados",
 				"tab": "Gestão de consignados",
 				"milestone": "Milestone 1",
 				"function": "ComSol_FS",
-				"status": "Concluída",
+				"status": "Programação",
 				"expected": "Sim",
 				"activeStartedDate": "07/06/2017",
 				"minutesExpected": "238",
@@ -70,7 +84,7 @@ class GridController {
 				"problem?": "Não"
 			},
 			{
-				"serviceOrder": "1405799",
+				"serviceOrder": "1405796",
 				"description": "#PRG 1 - 833728 - Gestão de consignados - Gestão de consignados",
 				"tab": "Gestão de consignados",
 				"milestone": "Milestone 1",
