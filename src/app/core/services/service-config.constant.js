10f4b9aee0fc7344ab51c3a3a745d1f94d2f8a8c
André <andre.lorenz@philips.com>
2017-08-16 08:33:50 -0300

    Added report

--- src/app/core/services/service-config.constant.js
+++ src/app/core/services/service-config.constant.js
@@ -1,4 +1,4 @@
 export const serviceConfig = { 
-	// BACKEND_URL: `http://192.168.253.204:5000`
-	BACKEND_URL: `http://10.125.2.54:8784`
+	BACKEND_URL: `http://192.168.253.204:5000`
+	// BACKEND_URL: `http://10.125.2.54:8784`
 };
